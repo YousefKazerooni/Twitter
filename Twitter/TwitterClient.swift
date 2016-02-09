17d446f68f6636c12fd0cf8330db5c71968cf456
YousefKazerooni <gityousefkazerooni@gmail.com>
2016-02-08 21:42:02 -0800

    Working on twitter client page

---
+++ Twitter/TwitterClient.swift
@@ -0,0 +1,33 @@
+//
+//  TwitterClient.swift
+//  Twitter
+//
+//  Created by Yousef Kazerooni on 2/8/16.
+//  Copyright © 2016 Yousef Kazerooni. All rights reserved.
+//
+
+import UIKit
+import BDBOAuth1Manager
+
+class TwitterClient: BDBOAuth1SessionManager {
+    
+    let twitterBaseUrl = NSURL(string: "https://api.twitter.com")
+    let twitterConsumerKey = "tUnMs3fqqBNPqIcbi857FHb7t"
+    let twitterConsumerSecret = "VIfcaiXAZpl7GFVSU9rThbAYRITk537XoX4ynSFpNKZFN0UvKO"
+    
+    class TwitterClient: BDBOAuth1SessionManager {
+        
+        class var sharedInstance: TwitterClient {
+            struct Static {
+                static let instance = TwitterClient(
+                    baseURL: twitterBaseUrl,
+                    consumerKey: twitterConsumerKey,
+                    consumerSecret: twitterConsumerSecret)
+            }
+            
+            return Static.instance
+        }
+        
+    }
+
+}
